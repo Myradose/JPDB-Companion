453ac80cd0130338a8cffc92d78b34b1ea7a8a46
Alden Geipel <aldengeipel@gmail.com>
2023-05-30 15:28:35 -0400

    add local storage for faster initial retrieval

--- content.js
+++ content.js
@@ -3,49 +3,61 @@ function updateBadge() {
     chrome.runtime.sendMessage({dueItems: dueItems});
 }
   
-function insertDueCounts() {
-    // Check if the current URL matches the review page
-    if (window.location.href.includes('review')) {
-        // Get due items count from the main page
-        chrome.runtime.sendMessage({contentScriptQuery: "fetchDueItems"}, dueText => {
-            var reviewContainer = document.querySelector('.container.bugfix');
-            if (reviewContainer) {
-                // Create new div to hold our data
-                var dueCountsDiv = document.createElement('div');
-                dueCountsDiv.style.fontSize = "20px";
-                dueCountsDiv.style.fontWeight = "bold";
-                dueCountsDiv.style.textAlign = "center";
-                dueCountsDiv.style.marginBottom = "20px";
+function insertDueCounts(dueText) {
+    var reviewContainer = document.querySelector('.container.bugfix');
+    if (reviewContainer) {
+        // Create new div to hold our data
+        var dueCountsDiv = document.createElement('div');
+        dueCountsDiv.style.fontSize = "20px";
+        dueCountsDiv.style.fontWeight = "bold";
+        dueCountsDiv.style.textAlign = "center";
+        dueCountsDiv.style.marginBottom = "20px";
 
-                // Extract the counts from the dueText
-                var dueItemsMatches = dueText.match(/(\d+) due items \((\d+) vocabulary and (\d+) kanji\)/);
-                if (dueItemsMatches) {
-                    var dueItems = dueItemsMatches[1]; // total due items
-                    var dueVocab = dueItemsMatches[2]; // vocabulary due items
-                    var dueKanji = dueItemsMatches[3]; // kanji due items
+        // Extract the counts from the dueText
+        var dueItemsMatches = dueText.match(/(\d+) due items \((\d+) vocabulary and (\d+) kanji\)/);
+        if (dueItemsMatches) {
+            var dueItems = dueItemsMatches[1]; // total due items
+            var dueVocab = dueItemsMatches[2]; // vocabulary due items
+            var dueKanji = dueItemsMatches[3]; // kanji due items
 
-                    // Insert the counts with red color
-                    dueCountsDiv.innerHTML = `<span style='color:red;'>${dueItems}</span> due items (<span style='color:red;'>${dueVocab}</span> vocabulary and <span style='color:red;'>${dueKanji}</span> kanji)`;
+            // Insert the counts with red color
+            dueCountsDiv.innerHTML = `<span style='color:red;'>${dueItems}</span> due items (<span style='color:red;'>${dueVocab}</span> vocabulary and <span style='color:red;'>${dueKanji}</span> kanji)`;
 
-                    // If our div already exists, remove it
-                    var existingDiv = document.getElementById('dueCountsDiv');
-                    if (existingDiv) existingDiv.remove();
+            // If our div already exists, remove it
+            var existingDiv = document.getElementById('dueCountsDiv');
+            if (existingDiv) existingDiv.remove();
 
-                    // Assign an id to our new div and insert it at the top of the container
-                    dueCountsDiv.id = 'dueCountsDiv';
-                    reviewContainer.insertBefore(dueCountsDiv, reviewContainer.firstChild);
-                }
-            }
-        });
+            // Assign an id to our new div and insert it at the top of the container
+            dueCountsDiv.id = 'dueCountsDiv';
+            reviewContainer.insertBefore(dueCountsDiv, reviewContainer.firstChild);
+        }
     }
 }
 
 // Immediately update the badge and insert due counts on page load
 updateBadge();
-insertDueCounts();
+var prevDueCounts = localStorage.getItem('prevDueCounts');
+if (prevDueCounts && window.location.href.includes('review')) {
+    insertDueCounts(prevDueCounts);
+}
 
-// And update the badge and due counts every 5 seconds
+function fetchAndInsertDueCounts() {
+    // Check if the current URL matches the review page
+    if (window.location.href.includes('review')) {
+        // Get due items count from the main page
+        chrome.runtime.sendMessage({contentScriptQuery: "fetchDueItems"}, dueText => {
+            // Update the counts and store the latest information in local storage
+            insertDueCounts(dueText);
+            localStorage.setItem('prevDueCounts', dueText);
+        });
+    }
+}
+
+// Fetch the due counts once on page load
+fetchAndInsertDueCounts();
+
+// And fetch the due counts every 5 seconds
 setInterval(function() {
     updateBadge();
-    insertDueCounts();
+    fetchAndInsertDueCounts();
 }, 5000);
