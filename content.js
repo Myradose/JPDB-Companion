433fe081ab89e37446406c64160320b6afa7558c
Alden Geipel <aldengeipel@gmail.com>
2023-06-02 13:18:48 -0400

    add total reviews today

--- content.js
+++ content.js
@@ -15,7 +15,6 @@ function insertDueCounts(dueText) {
         dueCountsDiv.style.marginBottom = "20px";
 
         let dueCounts = [0, 0, 0];
-        console.log(dueText);
         var dueItemsMatches = dueText.match(/You have (\d+) due items \((\d+) vocabulary and (\d+) kanji\) and (\d+) new items \((\d+) vocabulary and (\d+) kanji\) available for review./);
         if (dueItemsMatches) {
             dueCounts = [dueItemsMatches[1], dueItemsMatches[2], dueItemsMatches[3]];
@@ -46,11 +45,70 @@ function insertDueCounts(dueText) {
     }
 }
 
+function insertTotalReviews(count) {
+    var statsContainer = document.querySelector('.container.bugfix');
+    if (statsContainer) {
+        // Create new div to hold our data
+        var totalReviewsDiv = document.createElement('div');
+        totalReviewsDiv.style.fontSize = "20px";
+        totalReviewsDiv.style.fontWeight = "bold";
+        totalReviewsDiv.style.textAlign = "center";
+        totalReviewsDiv.style.marginBottom = "20px";
+
+        // Insert the counts with red color
+        totalReviewsDiv.innerHTML = `<span style='color:green;'>${count}</span> total reviews today`;
+
+        // If our div already exists, remove it
+        var existingDiv = document.getElementById('totalReviewsDiv');
+        if (existingDiv) existingDiv.remove();
+
+        // Assign an id to our new div and insert it at the top of the container
+        totalReviewsDiv.id = 'totalReviewsDiv';
+        statsContainer.insertBefore(totalReviewsDiv, statsContainer.firstChild);
+    }
+}
+
 // Immediately update the badge and insert due counts on page load
 updateBadge();
 var prevDueText = localStorage.getItem('prevDueText');
 if (prevDueText && window.location.href.includes('review')) {
     insertDueCounts(prevDueText);
+} else if (window.location.href.includes('stats')) {
+    // Get today's date and ignore the time.
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    let count = 0;
+
+    fetch('https://jpdb.io/export/reviews.json')
+    .then(response => response.json())
+    .then(data => {
+        // Function to iterate over reviews and count those from today
+        function countTodayReviews(reviews) {
+            for (const review of reviews) {
+                const reviewDate = new Date(review.timestamp * 1000); // Convert Unix timestamp to JavaScript Date
+                reviewDate.setHours(0, 0, 0, 0);
+
+                if (reviewDate.getTime() === today.getTime()) {
+                    count++;
+                }
+            }
+        }
+
+        // Iterate over the 'cards_vocabulary_jp_en' array
+        for (const card of data.cards_vocabulary_jp_en) {
+            countTodayReviews(card.reviews);
+        }
+
+        // Iterate over the 'cards_kanji_keyword_char' array
+        for (const card of data.cards_kanji_keyword_char) {
+            countTodayReviews(card.reviews);
+        }
+
+        insertTotalReviews(count);
+    })
+    .catch(error => console.error('Error:', error));
+
 }
 
 function fetchAndInsertDueCounts() {
